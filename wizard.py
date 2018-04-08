8b32f3a1d15f13607b1f55748012fb3ad6a639e5
Martin Kopeček <github@smallhill.cz>
2018-04-08 16:36:56 +0200

    Progres bar implemented

--- wizard.py
+++ wizard.py
@@ -54,12 +54,19 @@ class TransformPage(QWizardPage):
     def initializePage(self):
         self.transform()
 
+    def file_len(self,fname):
+        with open(fname) as f:
+            for i, l in enumerate(f):
+                pass
+        return i + 1
+
     def transform(self):
-        
+
         sourcePath = self.field("sourcePath")
         sourceName = os.path.splitext(sourcePath)[0]
-
-        print(sourceName)
+        
+        sourceLines = self.file_len(sourcePath)
+        
         targetPath = sourceName + "_upraveno.kxx"
         logPath = sourceName + "_zmeny.csv"
 
@@ -67,24 +74,27 @@ class TransformPage(QWizardPage):
             with open(targetPath,"w",encoding="windows-1250") as targetFile:
                 with open(logPath,"w",encoding="windows-1250") as logFile:
 
-                    logFile.write("\"Typ záznamu\";\"Číslo záznamu\";\"Změna\";\"\"\n")
+                    logFile.write("\"Typ záznamu\";\"Číslo záznamu\";\"Změna\";\"\"\n")                    
 
-                    for line in sourceFile:
+                    for lineNum, line in enumerate(sourceFile):
 
                         line = line.strip()
                         
                         p = re.compile('^(G\/\#000\d)(\d+).*\\\\n')
                         match = p.match(line)
 
-                        if not match:
+                        if match:
+
+                            parts = line.split("\\n")                
+
+                            targetFile.write(parts[0])
+                            logFile.write("\"" + match.group(1) + "\";\"" + match.group(2) + "\";\"Původní:\";\"" + line.replace("\"","\"\"") + "\"\n")
+                            logFile.write(";;\"Nové:\";\"" + parts[0].replace("\"","\"\"") + "\"\n\n")
+                        
+                        else:
                             targetFile.write(line)
-                            continue
 
-                        parts = line.split("\\n")                
-
-                        targetFile.write(parts[0])
-                        logFile.write("\"" + match.group(1) + "\";\"" + match.group(2) + "\";\"Původní:\";\"" + line.replace("\"","\"\"") + "\"\n")
-                        logFile.write(";;\"Nové:\";\"" + parts[0].replace("\"","\"\"") + "\"\n\n")
+                        self.progress.setValue((lineNum + 1) / sourceLines * 100)
  
 if __name__ == '__main__':
     import sys
