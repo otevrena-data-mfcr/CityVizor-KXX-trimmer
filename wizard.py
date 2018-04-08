d17533836fad9458375c3eaa5c7e39f65a12b975
Martin Kopeček <github@smallhill.cz>
2018-04-08 16:17:39 +0200

    First commit

---
+++ wizard.py
@@ -0,0 +1,94 @@
+import sys
+import os
+import re
+from PyQt5.QtWidgets import QApplication, QWidget, QWizard, QWizardPage, QProgressBar, QVBoxLayout, QPushButton, QFileDialog, QLineEdit, QProgressBar
+from PyQt5.QtGui import QIcon
+
+
+class MagicWizard(QWizard):
+    def __init__(self, parent=None):
+        super(MagicWizard, self).__init__(parent)
+        self.addPage(LoadPage(self))
+        self.addPage(TransformPage(self))
+        self.setWindowTitle("KXX Remover")
+        self.resize(640,480)
+
+ 
+class LoadPage(QWizardPage):
+    def __init__(self, parent=None):
+        super(LoadPage, self).__init__(parent)
+
+        btnLoadSource = QPushButton('Načíst zdrojový soubor KXX', self)
+        btnLoadSource.clicked.connect(self.openFileNameDialog)
+        
+        self.textbox = QLineEdit(self)
+
+        layout = QVBoxLayout()
+        layout.addWidget(btnLoadSource)
+        layout.addWidget(self.textbox)
+
+        self.registerField("sourcePath*", self.textbox)
+
+        self.setLayout(layout)
+
+    def getSourcePath(self):
+        return self.textbox.text()
+
+    def openFileNameDialog(self):    
+        options = QFileDialog.Options()
+        fileName = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","KXX Files (*.kxx)", options=options)
+        if fileName:
+            self.textbox.setText(fileName[0])            
+
+class TransformPage(QWizardPage):
+    def __init__(self, parent=None):
+        super(TransformPage, self).__init__(parent)
+        
+        self.progress = QProgressBar(self)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.progress)
+
+        self.setLayout(layout)
+
+    def initializePage(self):
+        self.transform()
+
+    def transform(self):
+        
+        sourcePath = self.field("sourcePath")
+        sourceName = os.path.splitext(sourcePath)[0]
+
+        print(sourceName)
+        targetPath = sourceName + "_upraveno.kxx"
+        logPath = sourceName + "_zmeny.csv"
+
+        with open(sourcePath,"r",encoding="windows-1250") as sourceFile:
+            with open(targetPath,"w",encoding="windows-1250") as targetFile:
+                with open(logPath,"w",encoding="windows-1250") as logFile:
+
+                    logFile.write("\"Typ záznamu\";\"Číslo záznamu\";\"Změna\";\"\"\n")
+
+                    for line in sourceFile:
+
+                        line = line.strip()
+                        
+                        p = re.compile('^(G\/\#000\d)(\d+).*\\\\n')
+                        match = p.match(line)
+
+                        if not match:
+                            targetFile.write(line)
+                            continue
+
+                        parts = line.split("\\n")                
+
+                        targetFile.write(parts[0])
+                        logFile.write("\"" + match.group(1) + "\";\"" + match.group(2) + "\";\"Původní:\";\"" + line.replace("\"","\"\"") + "\"\n")
+                        logFile.write(";;\"Nové:\";\"" + parts[0].replace("\"","\"\"") + "\"\n\n")
+ 
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    wizard = MagicWizard()
+    wizard.show()
+    sys.exit(app.exec_())
